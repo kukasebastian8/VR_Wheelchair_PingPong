384f5687c0cfc9b2dd02cb95598378742cd4de38
Luca Irimie <lucaniri@umich.edu>
2024-12-11 00:49:28 -0500

    Register button logic added

---
+++ Assets/Scripts/GoToTutorial.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToTutorial : MonoBehaviour
+{
+   public void NextScene(string SceneName){
+      SceneManager.LoadScene(SceneName);
+   }
+}
